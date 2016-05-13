8c4967352483fe85aad3f9a262c31e797fee4705
Mathew <matypatty@gmail.com>
2016-05-13 13:45:22 +1200

    Initial commit

---
+++ app/src/main/java/com/example/mathew/collaborator/Config.java
@@ -0,0 +1,9 @@
+package com.example.mathew.collaborator;
+
+/**
+ * Created by Mathew on 12-May-16.
+ */
+public class Config
+{
+    public static String API_BASE = "http://www.collaborator.pw/api.php?Action=";
+}
